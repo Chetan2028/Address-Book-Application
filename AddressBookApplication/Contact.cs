4a280425f654fa8e4ce6b6ee799db7fe92f71e34
Chetan2028 <bmchetan2028@gmail.com>
2020-11-15 13:08:04 +0530

    [Chetan] Add . Added contacts with details

--- AddressBookApplication/Contact.cs
+++ AddressBookApplication/Contact.cs
@@ -6,6 +6,7 @@ namespace AddressBookApplication
 {
     class Contact
     {
+        //Variables
         private string firstName;
         private string lastName;
         private string address;
@@ -15,13 +16,68 @@ namespace AddressBookApplication
         private long mobileNumber;
         private string email;
 
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
         public string FirstName { get => firstName; set => firstName = value; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
         public string LastName { get => lastName; set => lastName = value; }
+
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>
+        /// The address.
+        /// </value>
         public string Address { get => address; set => address = value; }
+
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The city.
+        /// </value>
         public string City { get => city; set => city = value; }
+
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>
+        /// The state.
+        /// </value>
         public string State { get => state; set => state = value; }
+
+        /// <summary>
+        /// Gets or sets the zip.
+        /// </summary>
+        /// <value>
+        /// The zip.
+        /// </value>
         public int Zip { get => zip; set => zip = value; }
+
+        /// <summary>
+        /// Gets or sets the mobile number.
+        /// </summary>
+        /// <value>
+        /// The mobile number.
+        /// </value>
         public long MobileNumber { get => mobileNumber; set => mobileNumber = value; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
         public string Email { get => email; set => email = value; }
     }
 }
